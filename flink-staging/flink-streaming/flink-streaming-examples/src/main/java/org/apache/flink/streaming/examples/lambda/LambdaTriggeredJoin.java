ff07bed142e04430c7e6658413934bbeef27334e
Fay Beligianni <faybeligianni@gmail.com>
2015-03-20 11:02:26 +0100

    Triggered join of Batch and Streaming processing

---
+++ flink-staging/flink-streaming/flink-streaming-examples/src/main/java/org/apache/flink/streaming/examples/lambda/LambdaTriggeredJoin.java
@@ -0,0 +1,7 @@
+package org.apache.flink.streaming.examples.lambda;
+
+/**
+ * Created by fobeligi on 3/20/15.
+ */
+public class LambdaTriggeredJoin {
+}
---
+++ flink-staging/flink-streaming/flink-streaming-examples/src/main/java/org/apache/flink/streaming/examples/lambda/SyntheticDataGenerator.java
@@ -0,0 +1,7 @@
+package org.apache.flink.streaming.examples.lambda;
+
+/**
+ * Created by fobeligi on 3/19/15.
+ */
+public class SyntheticDataGenerator {
+}
